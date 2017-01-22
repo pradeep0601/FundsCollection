b59630a9014da5ce7c6404f2320510f2046054dd
Pradeep Kumar <pradeepk.pdp@gmail.com>
2017-01-23 01:02:09 +0530

    code changes for dashboard

--- src/main/webapp/resources/js/controller/fundsMainCtrl.js
+++ src/main/webapp/resources/js/controller/fundsMainCtrl.js
@@ -9,13 +9,31 @@ fundsApp.controller("fundsMainCtrl", ["$scope","fundsMainService",function($scop
 				function(successResp){
 					console.log("fundsMainCtrl : getFundDetails : executed successfully!");
 					$scope.fundDetails = successResp;
-					
+					$scope.changeProgressBar();
 				},
 				function(errResp){
 					console.log("errResp = "+errResp);
 					console.error("fundsMainCtrl : getFundDetails : Error while getting fund details ");
 				}
 				);
+	}
+	
+
+	$scope.changeProgressBar = function(){
+		
+		var fundProgressBar = document.getElementById("fundProgressBarId");
+		if(fundProgressBar == null){
+			return;
+		}
+		if($scope.fundDetails.FUNDCYCLE.isInProgress){
+			fundProgressBar.className = "progress-bar progress-bar-success progress-bar-striped active";
+			fundProgressBar.innerHTML = "Funding is in progress..";
+		}
+		else{
+			fundProgressBar.className = "progress-bar progress-bar-danger progress-bar-striped active";
+			fundProgressBar.innerHTML = "Funding have been stopped..";
+		}
+		
 	}
 	
 }]);
